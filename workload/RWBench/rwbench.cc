2a5d2ca608a5c57742ef65306ec3c1a9e50fc704
haobayuxi <whaohit@gmail.com>
2023-10-21 20:19:34 +0800

    update

--- workload/RWBench/rwbench.cc
+++ workload/RWBench/rwbench.cc
@@ -25,11 +25,7 @@ bool rwbench::read_unlock(int addr, int version, long long end_time,
                           int thread_id) {
   if (type == RWLOCK_TYPE::Lease) {
     // check lease
-    std::cout << "time=" << end_time - start_time << "lease=" << lease
-              << std::endl;
-    sleep(1);
     if (likely((end_time - start_time) <= lease)) {
-      std::cout << "fast unlock" << std::endl;
       return true;
     }
     // validate
